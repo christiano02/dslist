4fd348e186f8d217226468f3eab7463356378463
Christiano <christianonascimento02@hotmail.com>
2025-05-19 16:36:50 -0300

    Game repository, service, controller and DTO

---
+++ src/main/java/com/example/dslist/dto/GameMinDTO.java
@@ -0,0 +1,59 @@
+package com.example.dslist.dto;
+
+
+import com.example.dslist.entities.Game;
+
+import java.util.Objects;
+
+public class GameMinDTO {
+
+    private Long id;
+    private Integer year;
+    private String title;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+        title = entity.getTitle();
+        year = entity.getYear();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameMinDTO that = (GameMinDTO) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/example/dslist/service/GameService.java
@@ -0,0 +1,21 @@
+package com.example.dslist.service;
+
+import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.entities.Game;
+import com.example.dslist.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).toList();
+    }
+}
